00d058f163e948ec05384712b60bad63302466a0
Edwin Cardenas <eacardenase@unal.edu.co>
2024-02-15 18:49:12 -0500

    feat: setup new item form

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shoppy/data/categories.dart';
 
 class NewItem extends StatefulWidget {
   const NewItem({super.key});
@@ -10,11 +11,71 @@ class NewItem extends StatefulWidget {
 class _NewItem extends State<NewItem> {
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(
-        'The Form',
-        style: TextStyle(
-          color: Colors.white,
+    return Center(
+      child: Form(
+        child: Column(
+          children: [
+            TextFormField(
+              maxLength: 50,
+              decoration: const InputDecoration(
+                label: Text('Name'),
+              ),
+              validator: (value) {
+                return 'Demo...';
+              },
+            ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Expanded(
+                  child: TextFormField(
+                    decoration: const InputDecoration(
+                      label: Text('Quantity'),
+                    ),
+                    initialValue: '1',
+                  ),
+                ),
+                const SizedBox(width: 8),
+                Expanded(
+                  child: DropdownButtonFormField(
+                    items: [
+                      for (final category in categories.entries)
+                        DropdownMenuItem(
+                          value: category.value,
+                          child: Row(
+                            children: [
+                              Container(
+                                width: 16,
+                                height: 16,
+                                color: category.value.color,
+                              ),
+                              const SizedBox(width: 6),
+                              Text(category.value.title)
+                            ],
+                          ),
+                        )
+                    ],
+                    onChanged: (value) {},
+                  ),
+                )
+              ],
+            ),
+            const SizedBox(height: 12),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                TextButton(
+                  onPressed: () {},
+                  child: const Text('Reset'),
+                ),
+                const SizedBox(width: 20),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: const Text('Save'),
+                )
+              ],
+            )
+          ],
         ),
       ),
     );
